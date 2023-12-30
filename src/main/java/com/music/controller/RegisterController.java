4b8c7e48554f507e8370bb90240c66f3b44727bb
TnjMywife <tnjmywife@gmail.com>
2023-12-30 18:04:01 +0800

    finally

--- src/main/java/com/music/controller/RegisterController.java
+++ src/main/java/com/music/controller/RegisterController.java
@@ -1,6 +1,8 @@
 package com.music.controller;
 
+import com.music.pojo.PlayList;
 import com.music.pojo.User;
+import com.music.service.playListService;
 import com.music.service.userService;
 
 import jakarta.annotation.Resource;
@@ -11,11 +13,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 
 @Controller
 public class RegisterController {
     @Resource
     private userService u;
+    @Resource
+    private playListService p;
     @RequestMapping("/user/regist")
     public String regist(@RequestParam("username") String userName,
                         @RequestParam("password") String password,
@@ -31,9 +39,7 @@ public class RegisterController {
         user.setGender(gender);
         user.setSignature("这个用户似乎有点懒");
 
-
         User flag = u.isUserUnique(userName);
-        /* 获取到提交的表单的用户信息后，验证用户名密码是否正确 */
         if(flag != null){
             model.addAttribute("userRepeat","Username already exists");
             return "register";
@@ -45,6 +51,21 @@ public class RegisterController {
         }
 
         int ret = u.save(user);
+        List<PlayList> playList=p.findAllPlayList();
+        int num=0;
+        for (PlayList list : playList) {
+            if (num < list.getListId()) {
+                num = list.getListId();
+            }
+        }
+
+        System.out.println(num);
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentTime.format(formatter);
+        p.addPlayList(num+1,"我喜欢",user.getUserId(),formattedDateTime);
+
+
         return "redirect:/index";
     }
 }
